7a01876685eab1855b71e7ab33b1ba7d5626ad67
Benedikt Radtke <benediktradtke@gmail.com>
2020-07-17 16:42:49 +0200

    dashboards dashboards

---
+++ run.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+docker-compose up -d elasticsearch kibana
+
+while ! curl -sq http://localhost:9200; do
+    echo "Waiting for elasticsearch to start...";
+    sleep 3;
+done
+while ! curl -sq --fail http://localhost:5601/api/status; do
+    echo "Waiting for kibana to start..."
+    sleep 3;
+done
+
+curl -vvv -X POST -F "file=@saved-obj.ndjson" -H 'kbn-xsrf: true' http://localhost:5601/api/saved_objects/_import?overwrite=True
+metricbeat setup --dashboards
+
+docker-compose up
